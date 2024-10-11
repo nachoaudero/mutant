7cbd519bcd3be9a2de08c11107fcd03cfeb485d2
nachoaudero <136350849+nachoaudero@users.noreply.github.com>
2024-10-10 21:45:11 -0300

    Creacion del README.md con explicacion de la API

--- src/main/java/com/example/mutant/controllers/DnaController.java
+++ src/main/java/com/example/mutant/controllers/DnaController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/mutante")
+@RequestMapping("/mutant")
 // Recibe el request del DTO, lo comunica con el Service y por ultimo envia el response con el formato del DTO
 public class DnaController {
     private final DnaService dnaService;
