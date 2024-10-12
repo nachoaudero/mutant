b7506fd87e92b8cdf3fdb3579943bdcaafbcec57
nachoaudero <136350849+nachoaudero@users.noreply.github.com>
2024-10-12 12:44:34 -0300

    Se agregaron los test unitarios y se corrigio el nombre del Dockerfile

---
+++ src/test/java/com/example/mutant/services/DnaServiceTests.java
@@ -0,0 +1,75 @@
+package com.example.mutant.services;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class DnaServiceTests {
+    // Test comprobando FILAS
+    @Test
+    public void testRows() {
+        String[] dna = {
+                "GGGG",
+                "TTTT",
+                "ACAC",
+                "CCAA"
+        };
+        assertTrue(DnaService.isMutant(dna));
+    }
+    // Test comprobando COLUMNAS
+    @Test
+    public void testColumns() {
+        String[] dna = {
+                "ACTT",
+                "ATTT",
+                "ACTC",
+                "CCTA"
+        };
+        assertTrue(DnaService.isMutant(dna));
+    }
+    // Test comprobando DIAGONAL PRINCIPAL
+    @Test
+    public void testMainDiagonal() {
+        String[] dna = {
+                "GGGG",
+                "TGTT",
+                "ACGC",
+                "CCAG"
+        };
+        assertTrue(DnaService.isMutant(dna));
+    }
+    // Test comprobando DIAGONALES SECUNDARIAS
+    @Test
+    public void testSecondDiagonal() {
+        String[] dna = {
+                "AGGGG",
+                "TTGTA",
+                "ACAGA",
+                "CCAAG",
+                "CTTAC"
+        };
+        assertTrue(DnaService.isMutant(dna));
+    }
+    // Test comprobando DIAGONAL INVERSA
+    @Test
+    public void testInverceDiagonal() {
+        String[] dna = {
+                "GGGG",
+                "TTGT",
+                "AGAC",
+                "GCAA"
+        };
+        assertTrue(DnaService.isMutant(dna));
+    }
+    // Test comprobando DNA HUMANO
+    @Test
+    public void testNoMutant() {
+        String[] dna = {
+                "GTTA",
+                "ACCC",
+                "ACAC",
+                "CCAA"
+        };
+        assertFalse(DnaService.isMutant(dna));
+    }
+}
